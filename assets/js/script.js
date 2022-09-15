35732106c3820beefa0ee19b3a9ac5e5c8026d58
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-09-15 16:54:30 -0400

    AllCharSet array will splice depending on prompt answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,6 @@ function isBoolean(val) {
   }
 }
 
-
-/*
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected 
-*/
-
 function promptConstraints() {
 
 //password length
@@ -62,15 +50,43 @@ function promptConstraints() {
       includeSc = prompt("include special characters?: ");
     }
   } while (includeLc == "false" && includeUc == "false" && includeNum == "false" && includeSc == "false" ); //checks to see if after running once, if at least one character set was selected
+  var includes = [includeLc, includeUc, includeNum, includeSc];
+  return (includes);
 }
 
 
 function generatePassword() {
-  promptConstraints();
+  var constraints = promptConstraints();
   
-  var allCharSets = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var allCharSets = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","1","2","3","4","5","6","7","8","9","0"," ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\]","^","_","`","{","|","}","~"];
+  if (constraints[3] == "false") { //specials
+    allCharSets.splice(62, 100 - 62 + 1);
+    console.log("special chars removed: "+ allCharSets)
+  }
+ 
+  if (constraints[2] == "false") { //numbers
+    allCharSets.splice(52, 62 - 52 + 1);
+    console.log("numbers removed: "+ allCharSets)
+  }
+   
+  if (constraints[1] == "false") { //uppercase
+    allCharSets.splice(26, 51 - 26 + 1);
+    console.log("Uppercase removed: "+ allCharSets)
+  }
+  if (constraints[0] == "false") { //lowercase
+    
+    allCharSets.splice(0, 25 - 0 + 1);
+    
+    console.log("Lowercase removed: "+ allCharSets)
+  }
+  var generatedPassword = allCharSets;
 
-  var generatedPassword = "x";
+
+  /*
+  remove from range if () 
+  includeLc == "false" && includeUc == "false" && includeNum == "false" && includeSc == "false"
+  */
+
   return generatedPassword;
 }
 
@@ -80,7 +96,7 @@ function generatePassword() {
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(includeLc, includeUc, includeNum, includeSc) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
