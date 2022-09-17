4edb5b14f5201b19eed5ff696019b9b350b52b40
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-09-16 23:57:29 -0400

    Changed to 2D array for sorting through the char sets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-function isBoolean(val) {
+function isBoolean(val) { //checks if a valid input has been inputted
   if (val == "true" || val == "false")
   {
     return true;
@@ -9,43 +9,61 @@ function isBoolean(val) {
   }
 }
 
-function promptConstraints() {
+function promptConstraints() { //Prompts the user for what constraints the password should use to generate
 
-//password length
+//Set password length 
   let passwordLength = 0;
   while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     passwordLength = prompt("Length of Password? (Valid between 8 & 128 characters): ");
+    if (passwordLength === null) {
+      return; //break out of the function early on cancel
+    }
   }
 
-  let includeLc = "";
-  let includeUc = "";
-  let includeNum = "";
-  let includeSc = "";
-  do {
+  //Variables to determine whether a character set should be included
+  let includeLc; //lowercase
+  let includeUc; //uppercase
+  let includeNum; //number
+  let includeSc; //special characters
+  
+  do { //checks to see if after running once if at least one character set was selected, otherwise reset and re-run
     includeLc = "";
     while (!isBoolean(includeLc))
     {
       includeLc = prompt("include lower case?: ");
+      if (includeLc === null) {
+        return; //break out of the function early on cancel
+      }
     }
 
     includeUc = "";
     while (!isBoolean(includeUc))
     {
       includeUc = prompt("include upper case?: ");
+      if (includeUc === null) {
+        return; //break out of the function early on cancel
+      }
     }
 
     includeNum = "";
     while (!isBoolean(includeNum))
     {
       includeNum = prompt("include numbers?: ");
+      if (includeNum === null) {
+        return; //break out of the function early on cancel
+      }
     }
 
     includeSc = "";
     while (!isBoolean(includeSc))
     {
       includeSc = prompt("include special characters?: ");
+      if (includeSc === null) {
+        return; //break out of the function early on cancel
+      }
     }
-  } while (includeLc == "false" && includeUc == "false" && includeNum == "false" && includeSc == "false" ); //checks to see if after running once, if at least one character set was selected
+
+  } while (includeLc == "false" && includeUc == "false" && includeNum == "false" && includeSc == "false" ); 
   var includes = [includeLc, includeUc, includeNum, includeSc, passwordLength];
   return (includes);
 }
@@ -54,50 +72,56 @@ function promptConstraints() {
 function generatePassword() {
   var constraints = promptConstraints();
   
-  var allCharSets = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","1","2","3","4","5","6","7","8","9","0"," ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\]","^","_","`","{","|","}","~"];
-  if (constraints[3] == "false") { //specials
-    allCharSets.splice(62, 100 - 62 + 1);
-    console.log("special chars removed: "+ allCharSets)
-  }
- 
-  if (constraints[2] == "false") { //numbers
-    allCharSets.splice(52, 62 - 52 + 1);
-    console.log("numbers removed: "+ allCharSets)
-  }
-   
-  if (constraints[1] == "false") { //uppercase
-    allCharSets.splice(26, 51 - 26 + 1);
-    console.log("Uppercase removed: "+ allCharSets)
-  }
-  if (constraints[0] == "false") { //lowercase
-    
-    allCharSets.splice(0, 25 - 0 + 1);
-    
-    console.log("Lowercase removed: "+ allCharSets)
+  var lowerCaseCharSet = "abcdefghijklmnopqrstuvwxyz";
+  var upperCaseCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberCharSet = "1234567890";
+  var specialCharSet = "~!@#$%^&*?/.";
+
+  var filteredConstraints = [];
+  var accountedFor = [];
+
+
+  var j = 0;
+  for (var i =0; i < constraints.length-1;i++) {
+    if (constraints[i] == "true") {
+      filteredConstraints[j] = i; // [j] = 0 to 4 if true
+      accountedFor[j] = false;
+      j++;
+    }
   }
 
+  //Two dimensional array
+  var allCharSets = [filteredConstraints, lowerCaseCharSet,upperCaseCharSet, numberCharSet, specialCharSet];
+  
   var generatedPassword =[];
-
+  var selectedIndex, selectedElement;
+  //First pass of generating the password
+  // wrap in do while until all needed types are accountedFor[]?
   for (var i =0; i < constraints[4];i++) {
-    generatedPassword[i] = allCharSets[(Math.floor(Math.random() * allCharSets.length+1))];
-  }
+    //                     = [random valid charset][random value in that valid charset]
+    //filteredConstraints[(Math.floor(Math.random() * filteredConstraints))]
 
+    selectedIndex = (Math.floor(Math.random() * filteredConstraints.length)); // randIndex chooses which array to pull from 
+    accountedFor[selectedIndex] = true;
+    
+    selectedElement = (Math.floor(Math.random() * allCharSets[filteredConstraints[selectedIndex]+1].length)); // randItem chooses an element inside the randomIndex's index
+
+    generatedPassword[i] = allCharSets[filteredConstraints[selectedIndex]+1]
+    [selectedElement];
+  }
+  
   return generatedPassword.join('');
 }
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-//includeLc, includeUc, includeNum, includeSc
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
